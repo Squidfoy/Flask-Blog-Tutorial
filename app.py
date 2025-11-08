b45fa02268f0dd40a1ea809495cfe4bb6dd73946
Squidfoy <alianna.monster@gmail.com>
2025-11-07 21:11:07 -0600

    Added Delete Functionality

--- app.py
+++ app.py
@@ -105,4 +105,29 @@ def edit(id):
 
     return render_template('edit.html', post=post)
 
+#create a route to delete a post
+#delete page will only be requested with a POST method
+#the post id is the url parameter
+@app.route('/<int:id>/delete/', methods=('POST',))
+
+def delete(id):
+    #get the post
+    post = get_post(id)
+
+    #connectto the database
+    conn = get_db_connection()
+
+    #execute a delete query
+    conn.execute('DELETE FROM posts WHERE id = ?', (id,))
+
+    #commit and close the connection
+    conn.commit()
+    conn.close()
+
+    #flash a sucess message
+    flash('"{}" was successfully deleted!'.format(post['title']))
+
+    #redirect to the home page
+    return redirect(url_for('index'))
+
 app.run(port=5008)
